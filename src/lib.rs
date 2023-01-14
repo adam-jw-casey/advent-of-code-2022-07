d39f617db17a8bdf375f6db4be0cfad514b7fba4
Adam Casey <adam.jw.casey@gmail.com>
2023-01-14 14:40:53 -0700

    Mocks in failing test

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use sscanf::sscanf;
+
+/// Sums the size of all directories with size of at most 100,000
+/// based on example terminal output
+/// # Examples
+/// ```
+/// use advent_of_code_2022_07::sum_dirs;
+/// use std::fs;
+///
+/// assert_eq!(sum_dirs(&fs::read_to_string("example_input.txt").unwrap()), 9543);
+/// ```
+pub fn sum_dirs(input: &String) -> usize{
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use advent_of_code_2022_07::sum_dirs;
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<_>>();
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read {file_path}");
+
+    println!("The size of all directories of at most 100,000 is: {}", sum_dirs(&contents));
 }
